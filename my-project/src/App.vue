78e01b4c2bf8d0d0f95c8f23356661b1cd8f27c0
Your Name <you@example.com>
2017-07-15 20:17:32 +0800

    firstVue02

---
+++ my-project/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div>
+      <router-link to="/">首页</router-link>
+      <router-link to="/about">about</router-link>
+      <router-link to="/xx">xx</router-link>
+    </div>
+    <router-view></router-view>
+    <foot></foot>
+  </div>
+</template>
+
+<script type="es6">
+import foot from './components/Hello'
+export default {
+  name: 'app',
+  components:{
+    foot
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ my-project/src/components/Hello.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="hello">
+   <ul class="bottomBox">
+     <li>西方世界</li>
+   </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: '一个新的项目'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
